9a8a4c220da36dedd8603ce350d6dfde3425e1db
Yelfery <ozoriayelfery@gmail.com>
2024-11-27 16:47:27 -0400

    Codigo del Programa

---
+++ src/modelo/Usuario.java
@@ -0,0 +1,95 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelo;
+
+/**
+ *
+ * @author ozori
+ */
+public class Usuario {
+
+    //Atributos
+    private int idUsuario;
+    private String nombre;
+    private String apellido;
+    private String usuario;
+    private String password;
+    private String telefono;
+    private int estado;
+
+    //Constructor
+    public Usuario() {
+        this.idUsuario = 0;
+        this.nombre = "";
+        this.apellido = "";
+        this.usuario = "";
+        this.password = "";
+        this.telefono = "";
+        this.estado = 0;
+    }
+
+    //set and get
+    public int getIdUsuario() {
+        return idUsuario;
+    }
+
+    public void setIdUsuario(int idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public int getEstado() {
+        return estado;
+    }
+
+    public void setEstado(int estado) {
+        this.estado = estado;
+    }
+
+    public void setUsurio(String trim) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+}
